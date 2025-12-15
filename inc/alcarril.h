7f9c960f7ce578b101cafc56b0906afc6ebf813b
alex <alexcarrillomc@outlook.com>
2025-12-15 17:33:25 +0100

    Minilibxsubmodule added

--- inc/alcarril.h
+++ inc/alcarril.h
@@ -6,11 +6,55 @@
 /*   By: alejandro <alejandro@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/15 03:14:57 by alejandro         #+#    #+#             */
-/*   Updated: 2025/12/15 03:14:59 by alejandro        ###   ########.fr       */
+/*   Updated: 2025/12/15 05:39:27 by alejandro        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ALCARRIL_H
 #define ALCARRIL_H
+
+/*
+	LIBRERIAS:
+*/
+
+#include "cube3D.h"
+
+/*
+	DEFINES:
+*/
+
+# define WIDTH 800
+# define HEIGHT 800
+
+/*
+	STRUCTS:
+*/
+
+typedef struct s_image_data
+{
+	void							*img_var;
+	char							*name;
+	char							*bit_map_address;
+	char							*bit_map_address_copy;
+	int								bits_per_pixel;
+	int								line_length;
+	int								endian;
+	char							**config;
+	int								origin[2];
+	double							zoom;
+	double							with[2];
+}									t_imgdata;
+
+typedef struct s_mlx_enviroment
+{
+	void							*mlx_var;
+	void							*window;
+	t_imgdata					*img_data;
+}				t_mlxenv;
+
+/*
+	FUNCTION PROTOTIPES:
+*/
+
 
 #endif
--- inc/cube3D.h
+++ inc/cube3D.h
@@ -6,7 +6,7 @@
 /*   By: alejandro <alejandro@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/15 03:15:05 by alejandro         #+#    #+#             */
-/*   Updated: 2025/12/15 03:15:06 by alejandro        ###   ########.fr       */
+/*   Updated: 2025/12/15 15:15:44 by alejandro        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,23 @@
 
 #include "alcarril.h"
 #include "carbon.h"
+
+/*
+	LIBRERIAS:
+*/
+
+//Self created APIs
+#include "libft/libft.h"
+#include "libft/get_next_line_bonus.h"
+//Sistem APIs
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/time.h>
+#include <math.h>
+//MiniLibx APIS
+// # include "../inc/Minilibx/mlx.h"
+// # include "../inc/Minilibx/mlx_int.h"
 
 #endif
